353ab82215af9e1c7e76975633250e7d45063585
YOO <graycuracao@gmail.com>
2025-04-29 10:32:26 +0900

    react에 웹캠 기능 추가

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import WebcamPage from "./pages/WebcamPage"; // ⭐ 추가
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/webcam" element={<WebcamPage />} /> {/* ⭐ 추가 */}
       </Routes>
     </Router>
   );
