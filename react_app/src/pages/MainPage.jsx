dc6a8dea2768df0726a9c3a1a653100a29257dab
YOO <graycuracao@gmail.com>
2025-04-29 10:41:27 +0900

    react에 웹캠 기능 추가2

--- react_app/src/pages/MainPage.jsx
+++ react_app/src/pages/MainPage.jsx
@@ -1,7 +1,14 @@
+// src/pages/MainPage.jsx
 import React from "react";
+import WebcamViewer from "../components/WebcamViewer";  // ✅ 정확히 가져오기
 
-function MainPage() {
-    return <h1>메인 페이지</h1>;
-}
+const MainPage = () => {
+  return (
+    <div>
+      <h1>메인 페이지</h1>
+      <WebcamViewer /> {/* ✅ 웹캠 컴포넌트 사용 */}
+    </div>
+  );
+};
 
 export default MainPage;
