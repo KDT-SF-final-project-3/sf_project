17d2a5450d3e77dac2e4f32b0e9dc914de311a25
BunnyByee <kimhime714@gmail.com>
2025-05-08 10:46:36 +0900

    [Fix] logout function

--- react_app/src/components/ProtectedRoute.jsx
+++ react_app/src/components/ProtectedRoute.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
 
-  if (isLoggedIn !== "true") {
-    // 로그인 상태가 아니면 로그인 페이지로 리다이렉트
+  if (!isLoggedIn) {
     return <Navigate to="/login" />;
   }
 
-  return children;  // 로그인 상태일 경우 자식 컴포넌트 렌더링
+  return children;
 };
 
 export default ProtectedRoute;
